2fcfc695a0f54bc3103803710a830e498ec41d09
SanketPatel77 <sanketpatel4872@gmail.com>
2025-03-04 16:33:43 +0530

    [Sanket] Add . validation for contact.

--- AddressBook/Contact.js
+++ AddressBook/Contact.js
@@ -1,7 +1,5 @@
 class Contact {
   constructor(firstName, lastName, address, city, state, zip, phone, email) {
-<<<<<<< HEAD
-=======
     if (!this.validateName(firstName)) throw new Error("Invalid First Name!");
     if (!this.validateName(lastName)) throw new Error("Invalid Last Name!");
     if (!this.validateAddress(address)) throw new Error("Invalid Address!");
@@ -12,18 +10,13 @@ class Contact {
       throw new Error("Invalid Phone Number!");
     if (!this.validateEmail(email)) throw new Error("Invalid Email!");
 
->>>>>>> UC2_Validation
     this.firstName = firstName;
     this.lastName = lastName;
     this.address = address;
     this.city = city;
     this.state = state;
     this.zip = zip;
-<<<<<<< HEAD
-    this.phone = phone;
-=======
     this.phoneNumber = phoneNumber;
->>>>>>> UC2_Validation
     this.email = email;
   }
 
@@ -31,8 +24,6 @@ class Contact {
     return `Name: ${this.firstName} ${this.lastName}, Address: ${this.address}, City: ${this.city}, 
     State: ${this.state}, Zip: ${this.zip}, Phone: ${this.phone}, Email: ${this.email}`;
   }
-<<<<<<< HEAD
-=======
 
   validateName(name) {
     let namePattern = /^[A-Z][a-zA-Z]{3,}$/;
@@ -58,5 +49,4 @@ class Contact {
     let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailPattern.test(email);
   }
->>>>>>> UC2_Validation
 }
