31eac042181ad829d5327b8b17f163eacda75993
SanketPatel77 <sanketpatel4872@gmail.com>
2025-03-04 16:57:48 +0530

    [Sanket] Add . Adding contact objects to array.

--- AddressBook/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-import Contact from "./Contact.js";
-import AddressBook from "./AddressBook.js";
-
-let addressBook = new AddressBook(); // reference of AddressBook class
-
-// create contacts
-let contact1 = new Contact(
-  "John",
